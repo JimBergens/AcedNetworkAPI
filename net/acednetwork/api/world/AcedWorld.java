aa204a2629a70a431a11e3faf46edf48d099c759
JimBergens <lolzzzzzmc@gmail.com>
2015-02-01 09:05:23 -0800

    Create AcedWorld

---
+++ net/acednetwork/api/world/AcedWorld.java
@@ -0,0 +1,41 @@
+package net.acednetwork.api;
+
+
+public class AcedWorld {
+
+private double x, y, z;
+private boolean valid;
+private int id;
+
+public double getX() {
+return x;
+}
+public double setX(double x) {
+return this.x = x;
+}
+public double getY() {
+return y;
+}
+public double setY(double y) {
+return this.y = y;
+}
+
+public double getZ() {
+return z;
+}
+public double setZ(double z) {
+return this.z = z;
+}
+public boolean isValid() {
+return valid;
+}
+public int getID() {
+return id;
+}
+public int setID(int id) {
+return this.id = id;
+}
+
+}
+
+
